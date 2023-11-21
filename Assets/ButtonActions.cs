529bf89ab1b03549f821ab6a791e316d776b0f89
Jan Benko <jan.benko777@gmail.com>
2023-11-21 15:48:29 +0100

    Plane detection, Simple main menu, Build testing

---
+++ Assets/ButtonActions.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonActions : MonoBehaviour
+{
+    void Start()
+    { }
+    void Update()
+    { }
+
+    public void LoadPlayScene()
+    {
+        SceneManager.LoadScene("Jan's Test Chamber");
+    }
+
+
+}
